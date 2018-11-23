82ead04f97a3590db500724116afb00363b498da
ParthBhindi <45254591+parth2020@users.noreply.github.com>
2018-11-23 09:42:14 +0530

    Add files via upload
    To print hello with  2 main

---
+++ multiple.java
@@ -0,0 +1,14 @@
+public class multiple{
+         public static void main(String [] args){
+                main(1);
+                main("java");
+       }
+      //method overridding
+      public static void main(int i){
+         System.out.println("Hello Tc3..");
+    }
+   //method overridding
+   public static void main(String str){
+     System.out.println("we are learning "+str);
+}
+}
