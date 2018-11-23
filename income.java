80f7a8fb2604b64c3b4022061465ec55beb55af0
ParthBhindi <45254591+parth2020@users.noreply.github.com>
2018-11-23 10:08:33 +0530

    Prog 3

---
+++ income.java
@@ -0,0 +1,21 @@
+public class income
+{
+	public static void main(String args[])
+	{
+		double inc=500.65,tax,rs;
+		int taxrs,taxpa;
+
+		System.out.println("Your Inncome: "+inc);
+
+		tax=(float)(inc*0.35);
+		System.out.println("Tax in USD: "+tax);
+	
+		rs=(float)(tax*70.46);
+
+		taxrs=(int)(rs);
+		System.out.println("Tax in India Rupees: "+taxrs);
+		
+		taxpa=(int)((rs-taxrs)*100);
+		System.out.println("Tax in India Paise: "+taxpa);
+	}
+}
