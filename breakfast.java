3ebc8dbb7a87e7e57cb74e6dc143220a3656d90a
ParthBhindi <45254591+parth2020@users.noreply.github.com>
2018-11-28 09:50:55 +0530

    Prog 5

---
+++ breakfast.java
@@ -0,0 +1,31 @@
+public class breakfast
+{
+	public static void main(String args[])
+	{
+		int br=0;
+		
+		br=(int)(Math.random()*6)
+
+		switch(br)
+		{
+			case 0: System.out.println("Today's Breakfast is Boiled Eggs");
+				break;
+
+			case 1: System.out.println("Today's Breakfast is Omlet");
+				break;
+			
+			case 2: System.out.println("Today's Breakfast is Ham Sanwich");
+				break;
+
+			case 3: System.out.println("Today's Breakfast is Whole Wheat Crepes");
+				break;
+
+			case 4: System.out.println("Today's Breakfast is Pancakes");
+				breaks;
+
+			case 5: System.out.println("Today's Breakfast is Chicken Burger");
+				break;
+		}
+	}
+}
+			
