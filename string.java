4ab857a6fb921b2e1616b6ebf235b898889f9a87
ParthBhindi <45254591+parth2020@users.noreply.github.com>
2018-12-28 13:05:58 +0530

    Prog 8

--- string.java
+++ string.java
@@ -1,17 +1,41 @@
-import java.util.Scanner
-
-public class string
-{
-	public static void main(String a[])
-	{
-		System.out.println("Enter String: ");
-		Scanner sc = new Scannner(System.in);
-		String s = sc.nextLine();
-
-		int l = (int)s.length();
-
-		String s = s.substring(1/2);
-
-		System.out.println("Last Half is: " "+s1+" "& Length is: " "+l+");
-	}
-}
+import java.util.Random; 
+ 
+public class string 
+{  
+	public static void main(String args[]) 
+	{   
+		int i; 
+ 
+		Random r = new Random();   
+		char c[] = new char[5]; 
+ 
+		for(i=0; i<5; i++)   
+		{    
+			c[i]=(char)(r.nextInt(26)+ 'A');   
+		} 
+ 
+		for(i=0; i<5; i++)   
+		{    
+			switch(c[i])    
+			{     
+				case 'A' : System.out.println("A Is Vowel....!!!");     
+				break;
+      
+    				case 'E' : System.out.println("E Is Vowel....!!!");     
+				break; 
+ 
+    				case 'I' : System.out.println("I Is Vowel....!!!");     
+				break; 
+ 
+    				case 'O' : System.out.println("O Is Vowel....!!!");     
+				break; 
+ 
+    				case 'U' : System.out.println("U Is Vowel....!!!");     
+				break; 
+ 
+    				default : System.out.println(c[i]);     
+				break;    
+			}   
+		}
+	 }
+} 
